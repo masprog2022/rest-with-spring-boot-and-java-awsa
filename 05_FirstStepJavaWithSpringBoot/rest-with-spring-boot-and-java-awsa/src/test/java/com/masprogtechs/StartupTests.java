1ef8433e9d2dacd0dbfe9f89d1042fa11393d619
masprog2022 <masprog2022@gmail.com>
2023-07-19 16:04:11 +0100

    :tada: Commit initial first Steps in Java with spring boot 3

---
+++ 05_FirstStepJavaWithSpringBoot/rest-with-spring-boot-and-java-awsa/src/test/java/com/masprogtechs/StartupTests.java
@@ -0,0 +1,13 @@
+package com.masprogtechs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
