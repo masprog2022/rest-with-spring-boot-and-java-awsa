2389deafc2ae14252f8be4494f95eabd14db2a23
masprog2022 <masprog2022@gmail.com>
2023-07-20 16:09:25 +0100

    :sparkles: feat: Parameters And ExceptionHandler

---
+++ 05_FirstStepJavaWithSpringBoot/rest-with-spring-boot-and-java-awsa/src/main/java/com/masprogtechs/math/SimpleMath.java
@@ -0,0 +1,37 @@
+package com.masprogtechs.math;
+
+
+
+public class SimpleMath {
+	
+	public Double sum(Double numberOne,Double numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	public Double subtraction( Double numberOne,Double numberTwo)  {
+		return numberOne - numberTwo;
+	}
+	
+	
+	public Double multiplcation(Double numberOne, Double numberTwo) {
+       return numberOne * numberTwo;
+	}
+	
+
+	public Double division(Double numberOne, Double numberTwo) {
+       return numberOne / numberTwo;
+	}
+	
+	public Double mean(Double numberOne, Double numberTwo) {
+	  return (numberOne + numberTwo) / 2;
+	}
+	
+
+
+	public Double squareRoot(Double number){
+		return Math.sqrt(number);
+	}
+
+
+
+}
