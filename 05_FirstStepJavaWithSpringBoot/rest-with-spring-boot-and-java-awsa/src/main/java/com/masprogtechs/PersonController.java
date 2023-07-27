61c7142a672627b34d2b9f23d4ab84c725ed0842
masprog2022 <masprog2022@gmail.com>
2023-07-27 12:03:06 +0100

    :sparkles: feat: implementando PersonVO e DozerMapper

--- 05_FirstStepJavaWithSpringBoot/rest-with-spring-boot-and-java-awsa/src/main/java/com/masprogtechs/PersonController.java
+++ 05_FirstStepJavaWithSpringBoot/rest-with-spring-boot-and-java-awsa/src/main/java/com/masprogtechs/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.masprogtechs.model.Person;
+import com.masprogtechs.data.vo.v1.PersonVO;
 import com.masprogtechs.services.PersonService;
 
 @RestController
@@ -27,26 +27,26 @@ public class PersonController {
 
 	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		
 		return service.findById(id);
 	}
 	
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll(){
+	public List<PersonVO> findAll(){
 		return service.findAll();
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person){
+	public PersonVO create(@RequestBody PersonVO person){
 		return service.create(person);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person){
+	public PersonVO update(@RequestBody PersonVO person){
 		return service.update(person);
 	}
 	
